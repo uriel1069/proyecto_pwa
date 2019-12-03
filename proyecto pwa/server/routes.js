8df462d4431af23ed1dcc424c4f5db715f9b992a
uriel1069 <58409321+uriel1069@users.noreply.github.com>
2019-12-02 22:14:42 -0600

    mandar las coordenadas a la consola

--- proyecto pwa/server/routes.js
+++ proyecto pwa/server/routes.js
@@ -24,10 +24,15 @@ router.get('/', function (req, res) {
 // Post mensaje
 router.post('/', function (req, res) {
   
-
+  console.log (req.body.lat );
+  console.log (req.body.lng );
+  
+  
   const mensaje = {
     mensaje: req.body.mensaje,
     user: req.body.user
+    lat: req.body.lat,
+    lng: req.body.lng
   };
 
   mensajes.push( mensaje );
